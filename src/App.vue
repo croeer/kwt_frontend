04b1ac4640b1194be9f17a266c2ffa3ffba602e9
croeer <christian@roeer.info>
2019-09-22 12:36:57 +0200

    working state

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img alt="logo" src="./assets/logo2.png" width=200>
+    <KittenWeights/>
+  </div>
+</template>
+
+<script>
+import KittenWeights from './components/KittenWeights.vue'
+import Chartkick from 'vue-chartkick'
+import Chart from 'chart.js'
+import Vue from 'vue'
+
+Vue.use(Chartkick.use(Chart))
+
+export default {
+  name: 'app',
+  components: {
+    KittenWeights
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/KittenWeights.vue
@@ -0,0 +1,61 @@
+<template>
+   <div id="app">
+    <line-chart :data="data" />
+
+    <ul>
+      <li v-for="(value, k) in weightDic" :key="k">
+        {{ k }}
+          <table>
+          <tr v-for="(v2, k2) in value" :key="k2"><td> {{ k2 }} </td><td>{{ v2 }} </td></tr>
+          </table>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'KittenWeights',
+  props: {
+    weightDic: {},
+    data: []
+  },
+    mounted() {
+    axios({ method: "GET",
+            url: "https://api.myjson.com/bins/busmh",
+            }).then(result => {
+        this.weightDic = result.data;
+        
+        this.data = [];
+        for (var k in this.weightDic) {
+          var l = {name: k, data: this.weightDic[k]};
+          this.data.push(l);
+        }
+          
+
+    }, error => {
+        this.errors.push(error);
+  });
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
